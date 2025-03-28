02d3c1df0728fa8cdad0f1459584e37b2faa2e8a
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2025-03-29 03:16:20 +0400

    modifying folder structure

--- server-automation/pcloud_backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-su - home_admin -c 'docker exec -t immich_postgres pg_dumpall -c -U postgres | gzip > "/mnt/storage/immich_db.sql.gz"' &&
-
-rsync -ah /mnt/pcloud/ /mnt/storage/
--- server-automation/restart_server.sh
+++ server-automation/restart_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mount /dev/sda1 /mnt/pcloud &&
+mount /dev/sda1 /mnt/immich &&
 mount /dev/sdb1 /mnt/storage &&
-mount /dev/sdc1 /mnt/vault
+mount /dev/sdc1 /mnt/media-library
