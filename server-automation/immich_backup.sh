e50ae29321d7e7579aed3a49851067a7d467dcc5
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2025-03-29 03:18:00 +0400

    small changes

--- server-automation/immich_backup.sh
+++ server-automation/immich_backup.sh
@@ -2,4 +2,4 @@
 
 su - home_admin -c 'docker exec -t immich_postgres pg_dumpall -c -U postgres | gzip > "/mnt/storage/backups/immich/immich_db.sql.gz"' &&
 
-rsync -ah /mnt/immich/ /mnt/storage/backups/immich/
+rsync -ah /mnt/immich/ /mnt/storage/backups/immich/immich/
