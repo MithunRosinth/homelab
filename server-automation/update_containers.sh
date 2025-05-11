0e4ab0a5bac0a1b44a387f831a586ac118d24fff
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2025-05-11 16:44:16 +0400

    adding docker update script

---
+++ server-automation/update_containers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/home_admin/immich-app &&
+docker compose down &&
+docker compose pull &&
+docker compose up -d &&
+
+cd /home/home_admin/homelab/server-helm &&
+docker compose down &&
+docker compose pull &&
+docker compose up -d &&
+
+cd /home/home_admin/homelab/media-server &&
+docker compose down &&
+docker compose pull &&
+docker compose up -d &&
+
+
+docker image prune -f
