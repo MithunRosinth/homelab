6b4d96e13c80c7244479a3130482ebc93f7d4174
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2025-05-15 22:37:10 +0400

    removing unecessary scripts

--- server-automation/restart_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run the root script
-/home/home_admin/restart_server.sh  &&
-
-# Run the home_admin script
-su - home_admin -c "/home/home_admin/restart_docker.sh"
--- server-automation/restart_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mount /dev/sda1 /mnt/immich &&
-mount /dev/sdb1 /mnt/storage &&
-mount /dev/sdc1 /mnt/media-library
