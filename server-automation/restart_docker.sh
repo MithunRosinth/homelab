788e3e01fa9e90aa29fa81ca801c9fecdb8b6605
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2025-06-08 08:17:18 +0400

    adding home to automation

--- server-automation/restart_docker.sh
+++ server-automation/restart_docker.sh
@@ -10,4 +10,8 @@ docker compose up -d &&
 
 cd /home/home_admin/homelab/media-server &&
 docker compose down &&
+docker compose up -d &&
+
+cd /home/home_admin/homelab/homepage &&
+docker compose down &&
 docker compose up -d 
--- server-automation/update_containers.sh
+++ server-automation/update_containers.sh
@@ -15,5 +15,9 @@ docker compose down &&
 docker compose pull &&
 docker compose up -d &&
 
+cd /home/home_admin/homelab/homepage &&
+docker compose down &&
+docker compose pull &&
+docker compose up -d &&
 
 docker image prune -f
